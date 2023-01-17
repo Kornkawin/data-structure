53af866ab71de15933e3f3c1ad5d1e9d1c5fa77c
Kornkawin Bijayayothin <outsource1456@setth.onmicrosoft.com>
2023-01-17 10:31:39 +0700

    [HashTable] add simple example

---
+++ src/main/java/com/dsa/hashtable/HashTable.java
@@ -0,0 +1,49 @@
+package com.dsa.hashtable;
+
+import com.dsa.Set;
+
+public class HashTable implements Set {
+    private Object[] table;
+    private int size;
+
+    public HashTable(int m) {
+        table = new Object[m];
+        size = 0;
+    }
+
+    @Override
+    public int size() {
+        return size;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    @Override
+    public void remove(Object element) {
+        if(table[hash(element)] != null && table[hash(element)].equals(element)) {
+            table[hash(element)] = null;
+            size--;
+        }
+    }
+
+    @Override
+    public boolean contains(Object element) {
+        return table[hash(element)] != null && table[hash(element)].equals(element);
+    }
+
+    @Override
+    public void add(Object element) {
+        if(table[hash(element)] == null) {
+            table[hash(element)] = element;
+            size++;
+        }
+    }
+
+    private int hash(Object x) {
+        // depend on algorithm designs
+        return x.hashCode() % table.length;
+    }
+}
